2e7bf3ee8d182bad2ebca2edcd9e064b3e349f60
felipe pupo rodrigues <fpupor@gmail.com>
2013-02-07 11:56:07 -0200

    fsdafsa
    fsafdsda

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 deviceready.push(function(){
+	alert('ready');
 	MyApp = new gapVersion({
 		SERVER: 'http://hml.conheca.me/gapversion/',
 		SYSTEM: 'system.php',
@@ -14,10 +15,14 @@ deviceready.push(function(){
 	}, false);
 	
 	document.addEventListener("online", function(){
+		alert('on');
 		MyApp.online();
 	}, false);
 	
 	document.addEventListener("offline", function(){
+		alert('off');
 		MyApp.offline();
 	}, false);
+	
+	alert('ready end');
 });
