10c40970944376f466161b2f1a2b66cf20e94baf
felipe pupo rodrigues <fpupor@gmail.com>
2013-02-07 10:10:07 -0200

    Retest
    ...

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 alert('init run');
 
-document.addEventListener("deviceready", function(){
-	alert('ready');
-	
-	MyApp = new gapVersion({
-		SERVER: 'http://hml.conheca.me/gapversion/',
-		SYSTEM: 'system.php',
+MyApp = new gapVersion({
+	SERVER: 'http://hml.conheca.me/gapversion/',
+	SYSTEM: 'system.php',
 
-		onReady: function(){
-			such.DEBUG.info('app ok');
-		}
-	});
+	onReady: function(){
+		such.DEBUG.info('app ok');
+	}
+});
+
+
+document.addEventListener("deviceready", function(){
+
+	alert(MyApp);
 	
 	MyApp.checkVersion();
+	
+	alert('runat');
 }, false);
-
-alert('runat');
