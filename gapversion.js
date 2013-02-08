a9dd9ba045a96d0a997db44f712c9d54feda7f31
felipe pupo rodrigues <fpupor@gmail.com>
2013-02-08 16:19:40 -0200

    dsa
    sadfasd

--- gapversion.js
+++ gapversion.js
@@ -160,7 +160,7 @@
 					
 						such.errorHandler('nao encontrou arquivo', e);
 						
-						such.downloadFile(filePath + (filePath.charAt(filePath.length) != '/' ? '/' : '') + such.UPDATES.version + '/' + fileName, 'Assets/' + filePath, function(fileEntry){
+						such.downloadFile(such.UPDATES.version.toFixed(1) + '/' +filePath + fileName, 'Assets/' + filePath, function(fileEntry){
 							alert('saved file');
 						}, function(e){
 							such.errorHandler('nao baixou arquivo', e);
