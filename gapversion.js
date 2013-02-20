9b81a84c871db32d4e75743a9abeef6f4ac1a044
felipe pupo rodrigues <fpupor@gmail.com>
2013-02-20 12:45:46 -0300

    fgsad
    sfdafasdfa

--- gapversion.js
+++ gapversion.js
@@ -251,7 +251,10 @@
 					such.DEBUG.info('open file version');
 					such.UPDATES = such.parseProtocol(file.target.result);
 					
-					if(such.options.onCheckVersion(such.VERSION == NaN || such.VERSION == 0 || such.UPDATES.version > such.VERSION))
+					var updateNow = such.VERSION == NaN || such.VERSION == 0 || such.UPDATES.version > such.VERSION;
+					such.DEBUG.info('open file version '+updateNow+' last:'+ such.VERSION + ' new:' + such.UPDATES.version);
+					
+					if(such.options.onCheckVersion())
 						such.updateVersion();
 				}, function(e){
 					such.errorHandler('open file version' , e);
