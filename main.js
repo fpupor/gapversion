cb83f31ce3ed25f7f0bfc23ef459b856a6d8fc72
felipe pupo rodrigues <fpupor@gmail.com>
2013-03-14 18:52:52 -0300

    fsad
    fsadfasd

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ deviceready.push(function(){
 					MyApp.openFile(fileEntry, function(file){
 						
 						MyApp.DEBUG.info('inner content html');
-						document.getElementById('body').innerHTML = file.target.result;
+						document.getElementById('body').innerHTML = "sadffdsafa"//file.target.result;
 						
 						Loader.js(MyApp.FILESYSTEM.fullPath + '/Assets/js/init.js', function(){
 							MyApp.DEBUG.info('init.js include');
